7a5ce818db602a2b2a09280c08b89c499ea3ed04
b j ø r n <bjoern@holthoefer.de>
2026-05-18 21:26:38 +0200

    Ich bin Björn, den ihr schon so lange kennt, Ich bin am Start, wenn ihr meinen Namen nennt, Ich will eigentlich nur von der Dunkelheit erzählen, Ansonsten brauch ich eigentlich nichts. Ich bin Lori, bin grad mal 13 Jahre alt Und jeder sagt, ich bin schon sehr, sehr alt, Ich komm aus Italien Das ist mein Land! Und ich bi, (hier hat er sich "verrappt!") und ich chill den ganzen Tag nur am Strand. Ich bin 13 Jahre alt und aus dem Kosovo, Jeder sagt meine Zukunft ist hoffnungslos, Doch das ist mein Ding und ihr seid live dabei, Ich bin tight am mic, ohne Scheiß! Ohne Scheiß! Ich bin Dragan, ich komme aus Großhepbach, Und wenn ich rappe, dann glaub ich, dass es scheppert! Jetzt wisst ihr wer ich bin; kommt, schreibt meinen Namen, Dra zu dem g-a-n, schreibt ihn zusammen!
    5 Jungs das ist Rap aus Waiblingen!
    Leute hüpfen hin und her, wie beim Seilspringen,
    5 Jungs das ist Rap aus Waiblingen!
    Guck sie dir an und hör wie sie tight klingen!
    5 Jungs das ist Rap aus Waiblingen!
    Leute hüpfen hin und her, wie beim Seilspringen,
    5 Jungs das ist Rap aus Waiblingen!
    Guck sie dir an und hör wie sie tight klingen!
    Bin jung, talentiert, und auch motiviert,
    Ich gehe meinen Weg egal wer mich kritisiert,
    Ich geb alles was ich kann, wenns mich interessiert,
    Ganz egal was kommt ich bleibe hier!
    Ich bin ein Albaner, bin kreativ,
    Die Lehrer hier finden mich viel zu negativ,
    Doch ich weiß, dass ich hierbleib, für immer hierbleib,
    Und ich schreib es auf in diesen vier Zeilen Yeah!
    Ihr denkt, dass ich ein harter Junge bin,
    Doch ich schreibe Rimes wenn ich keine ruhige Stunde find,
    Jo, ich bin dieser Türke der dich immer wieder disst,
    weißt du wer du bist, du bist ein nichts!
    Ich schrei es um die Welt aus dem Ameisenhügel,
    wenn du etwas schreibst, spür ich gar kein Gefühl Yo!
    Ich schmeiß meine rhimes wie beim Handball,
    Und es wird laut, als ob du an die Wand knallst.
    Ich bin B-J-ööörn,
    Guck mich an wie ich alles wegburn,
    Alles wegburn ta!
    Ich fang grad erst an mit Rap
    und wenn ich es kann, puste ich dich einfach weg!
    Du lachst jetzt nur noch noch über mich,
    doch später siehst du überall nur mein Gesicht, überall nur mein Gesicht,
    Das sind Merk und Arian, L-O-R-I,
    Björn und Dragan, yeah, thats me!
    
    5 Jungs das ist Rap aus Waiblingen!
    Leute hüpfen hin und her, wie beim Seilspringen,
    5 Jungs das ist Rap aus Waiblingen!
    Guck sie dir an und hör wie sie tight klingen!
    5 Jungs das ist Rap aus Waiblingen!
    Leute hüpfen hin und her, wie beim Seilspringen,
    5 Jungs das ist Rap aus Waiblingen!
    Guck sie dir an und hör wie sie tight klingen!
    Das waren wir: 5 Jungs aus Waiblingen,
    Dragan, Merk, Lori, Adrian, Björn,
    Und seht es alle, wie wir alles jetzt wegburnen!
    Yeah! Ha Ha!

---
+++ src/automated_hyperparameter_tuning/prototype/ConfigConfigurator.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from datetime import datetime
+import time
+
+from pandas._libs import algos
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+from ConfigLoader import ConfigLoader
+
+
+def main():
+    print("Config Configuration gestartet")
+
+    config_path = os.path.join(
+        os.path.dirname(__file__), "..", "..", "..", "resources", "configs", "config.toml"
+    )
+
+    config_loader = ConfigLoader(config_path)
+    config        = config_loader.config
+
+    algorithm = config["Algorithm"]
+
+    # Festlegen der initialen parameter hier, so müssen die nicht von Gruppe 1 angegeben werden
+    if algorithm == "knn":
+
+        grenzeLayer1 = abs(config["Data"]["input_start"] - config["Data"]["input_end"] - 1)
+        grenzeLayer2 = config["Data"]["input_end"] - config["Data"]["target_column"]
+
+        search_space = {
+            "layer1": [1, grenzeLayer1],
+            "layer2": [1, 500],
+            "layer3": [2, grenzeLayer2],
+            "activation": [ "relu", "tanh", "sigmoid",],
+            "learning_rate": [ 1e-5, 1.0,],
+            "epochs": [1, 500,],
+            "loss_function": [ "mse", "bce", "mae"]
+        }
+
+        config_loader.config["NeuralNetwork"]["SearchSpace"] = search_space
+        config_loader.save(config_path)
+
+    if algorithm == "logistic_regression":
+        search_space = {
+            "C": [0.001, 100.0],
+            "max_iter": [100, 1000],
+            "solver": ["lbfgs", "saga"],
+            "penalty": ["l2", "none"]
+        }
+        config_loader.config["LogisticRegression"]["SearchSpace"] = search_space
+        config_loader.save(config_path)
+
+    if algorithm == "svm":
+        search_space = {
+            "C" : [0.01, 100.0, ],
+            "kernel" : ["linear", "rbf", "poly", "sigmoid", ],
+            "gamma" : ["scale", "auto", ]
+        }
+        config_loader.config["SVM"]["SearchSpace"] = search_space
+        config_loader.save(config_path)
+
+    if algorithm == "knn":
+        search_space = {
+            "n_neighbors" : [1, 50, ],
+            "weights" : ["uniform", "distance", ],
+            "metric" : ["euclidean", "manhattan", "minkowski", ]
+        }
+        config_loader.config["KNN"]["SearchSpace"] = search_space
+        config_loader.save(config_path)
